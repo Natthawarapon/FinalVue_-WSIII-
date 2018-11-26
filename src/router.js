57ddc4465c1c93142c228c5f34d631e02a04e251
din007 <kurruna@gmail.com>
2018-11-26 18:14:06 +0700

    Get product from api

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Products from './views/Products.vue'
+import Users from './views/Users.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/products',
       name: 'products',
       component: Products
+    },
+    {
+      path: '/users',
+      name: 'users',
+      component: Users
     },
     {
       path: '/about',
